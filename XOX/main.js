4533b46f2d134a02578cc7fa581fc8a871c95a4e
Oliver <olive@DESKTOP-R044BGP>
2022-01-07 14:05:06 +0100

    FIxed document load issue

--- XOX/main.js
+++ XOX/main.js
@@ -3,9 +3,19 @@
 window.addEventListener("onload", run());
 
 function run() {
-    
-    const root = document.getElementById("root");
-    const table = document.createElement("table");
 
+    const root = document.getElementById("root");
+    const table = document.getElementById("table");
+
+    console.log(root);
+    
+    for (let i = 0; i < 3; i++) {
+        const tr = document.createElement("tr");
+        table.appendChild(tr);
+        for (let j = 0; j < 3; j++) {
+            const td = tr.insertCell();
+            td.innerText = "X";
+        }
+    }
 
 }
