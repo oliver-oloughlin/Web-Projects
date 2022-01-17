2893338ba7f6cb880410bd1b803c1bfc83f1b318
h584974 <oliveroloughlin@hotmail.com>
2022-01-17 16:11:17 +0100

    Game is winnable

--- XOX/main.js
+++ XOX/main.js
@@ -3,11 +3,25 @@
 import GameController from "./GameController.js"
 
 window.onload = run;
+let gc;
 
 function run() {
 
     const table = document.getElementById("table");
-    const gc = new GameController(table);
+    const board = [];
+    for (let i = 0; i < 3; i++) {
+        const tr = document.createElement("tr");
+        table.appendChild(tr);
+        const row = [];
+        for (let j = 0; j < 3; j++) {
+            const td = tr.insertCell();
+            td.onclick = (e) => gc.makeMove(e.target);
+            row.push(td);
+        }
+        board.push(row);
+    }
+
+    gc = new GameController(board);
     gc.start();
     
 }
